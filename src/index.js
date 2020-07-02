27225989c1b7603324452e5fd78a27e723db0802
Vasyl <vsmentvs@gmail.com>
2020-07-02 19:18:20 +0300

    clock, events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Clock} from "./clock";
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {inputValue: ""};
+    }
+
+    onChange = event => this.setState({inputValue: event.target.value})
+
+    onClick = () => {
+        this.props.onAdd(this.state.inputValue);
+        this.setState({inputValue: ""});
+    }
+
     render() {
         return (
-            <h1 className="header">{this.props.title}</h1>
+            <Fragment>
+                <h1 className="header">{this.props.title}</h1>
+                <div>
+                    <input type="text" value={this.state.inputValue} onChange={this.onChange}/>
+                    <button onClick={this.onClick}>Add</button>
+                </div>
+            </Fragment>
         );
     }
 }
@@ -21,15 +40,18 @@ class Body extends React.Component {
 }
 
 Body.defaultProps = {todoList: []};
-Body.displayName="TODO LIST";
+Body.displayName = "TODO LIST";
 
 class App extends React.Component {
     state = {todos: ['1-st', '2-nd', '3-rd']};
 
+    onAdd = todo => this.setState({todos: [...this.state.todos, todo]});
+
     render() {
         return (
             <div className="app">
-                <Header title="This is  TODO list"/>
+                <Clock/>
+                <Header onAdd={this.onAdd} title="This is  TODO list"/>
                 <Body todoList={this.state.todos}/>
             </div>
         )
