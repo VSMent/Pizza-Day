fbd5f60dd6c280506d48dc3f2970667af8d1ed26
Vasyl <vsmentvs@gmail.com>
2020-07-02 16:04:36 +0300

    components, props, state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <h1 className="header">{this.props.title}</h1>
+        );
+    }
+}
+
+class Body extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                {this.props.todoList.map(todo => <div key={todo}>{todo}</div>)}
+            </Fragment>
+        );
+    }
+}
+
+Body.defaultProps = {todoList: []};
+Body.displayName="TODO LIST";
+
+class App extends React.Component {
+    state = {todos: ['1-st', '2-nd', '3-rd']};
+
+    render() {
+        return (
+            <div className="app">
+                <Header title="This is  TODO list"/>
+                <Body todoList={this.state.todos}/>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
