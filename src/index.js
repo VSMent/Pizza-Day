a2086cdb3a1cee92bf8cfe3e0511c6d078552fc5
Vasyl <vsmentvs@gmail.com>
2020-07-05 19:17:44 +0300

    rewrite index (App) component to functional component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Clock} from "./clock";
@@ -6,28 +6,28 @@ import {Header} from "./header";
 import {Body} from "./body.js";
 
 
-class App extends React.Component {
-    state = {todos: ['1-st', '2-nd', '3-rd'], showClock: true};
+const App = () => {
+    const [app, setApp] = useState({
+        todos: ['1-st', '2-nd', '3-rd'],
+        showClock: true
+    });
 
-    onAdd = todo => this.setState({todos: [...this.state.todos, todo]});
+    const onAdd = todo => setApp({...app, todos: [...app.todos, todo]});
+    const onSwitch = () => setApp({...app, showClock: !app.showClock});
 
-    onSwitch = () => this.setState({showClock: !this.state.showClock});
+    const {todos, showClock} = app;
 
-    render() {
-        const {todos, showClock} = this.state;
-
-        return (
-            <div className="app">
-                <button onClick={this.onSwitch}>
-                    Turn Clock {showClock ? 'Off' : 'On'}
-                </button>
-                {showClock && <Clock/>}
-                <Header onAdd={this.onAdd} title="This is  TODO list"/>
-                <Body todoList={todos}/>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="app">
+            <button onClick={onSwitch}>
+                Turn Clock {showClock ? 'Off' : 'On'}
+            </button>
+            {showClock && <Clock/>}
+            <Header onAdd={onAdd} title="This is  TODO list"/>
+            <Body todoList={todos}/>
+        </div>
+    );
+};
 
 ReactDOM.render(
     <App/>,
