9da65c530b812d5d487a0238f4b882ad7880e47e
VSMent <vsmentvs@gmail.com>
2020-07-20 10:45:14 +0300

    basic routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import './index.css';
 
+const App = () => (
+  <BrowserRouter>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/products">Products</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path="/" render={() => <div>Home Page</div>}/>
+      <Route path="/products" render={() => <div>Products</div>}/>
+      <Route path="/about" render={() => <div>About</div>}/>
+    </Switch>
+  </BrowserRouter>
+);
+
 ReactDOM.render(
-  <div>
-    APP
-  </div>,
+  <App/>,
   document.getElementById('root')
 );
