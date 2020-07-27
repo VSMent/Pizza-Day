077592ec67aa212c1ce5d3f58ea6aefed03a6c38
VSMent <vsmentvs@gmail.com>
2020-07-27 18:45:16 +0300

    add limit, skip to question list function

--- src/controllers/questions/list.js
+++ src/controllers/questions/list.js
@@ -1,7 +1,13 @@
 const list = ({Question}) => async (req, res, next) => {
   console.log("--> Questions: GET");
   try {
-    const questions = await Question.find({});
+    let {limit, skip} = req.query;
+    limit = limit ? parseInt(limit) : 20;
+    skip = skip ? parseInt(skip) : 0;
+    const questions = await Question.find({})
+      .skip(skip)
+      .limit(limit);
+
     res.status(200).send({questions});
   } catch (e) {
     next(e);
